2660e19bbcde3d013569c0d8044b8ab62fc96896
Igor Henrique <Igor Henrique@duetto.srv>
2022-05-02 14:11:53 -0300

    Inclusão da classe - sticky

--- application/views/dashboard/header.php
+++ application/views/dashboard/header.php
@@ -72,9 +72,6 @@
                         <li class="nav-item">
                             <?php echo anchor('galeria', '<span><i class="fas fa-laptop-code"></i> Galeria</span>', array('class' => 'nav-link')) ?>
                         </li>
-                        <li class="nav-item">
-                            <?php echo anchor('contato', '<span><i class="fas fa-laptop-code"></i> Log de Contato</span>', array('class' => 'nav-link')) ?>
-                        </li>
                     </ul>
                     <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                         <span>Cadastros</span>
